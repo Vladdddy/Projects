c65085ea887bb14fbe9c67564980ed9159d9f6bb
vladeubanks <103177221+Vladdddy@users.noreply.github.com>
2024-11-23 12:13:42 +0100

    typescript

---
+++ calendar/typescript.ts
@@ -0,0 +1,6 @@
+const send = document.getElementById("send") as HTMLElement;
+const calendar = document.getElementById("calendar") as HTMLElement;
+
+send?.addEventListener("click", () => {
+    send.style.background = "black";
+});
