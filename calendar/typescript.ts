4648b2c31bdf0a2f8ba1d2dfe1479a3e169ea999
Vladyslav Bukator <103177221+Vladdddy@users.noreply.github.com>
2024-11-27 11:59:19 +0100

    Delete calendar directory

--- calendar/typescript.ts
+++
@@ -1,6 +0,0 @@
-const send = document.getElementById("send") as HTMLElement;
-const calendar = document.getElementById("calendar") as HTMLElement;
-
-send?.addEventListener("click", () => {
-    send.style.background = "black";
-});
