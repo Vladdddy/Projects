ddedcafb952a45676eba625b865c387255ef032e
vladyslav <vlad00anonimous00@gmail.com>
2024-12-29 17:47:43 +0100

    homepage done

---
+++ samsung-galaxy-book/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const images = document.querySelectorAll('.image');
+    const dots = document.querySelectorAll('.index-box .dot, .index-box .full-dot');
+    let currentIndex = 0;
+
+    function showImage(index) {
+        images.forEach((img, i) => {
+            img.style.display = i === index ? 'flex' : 'none';
+        });
+
+        dots.forEach((dot, i) => {
+            dot.className = i === index ? 'full-dot' : 'dot';
+        });
+    }
+
+    function nextImage() {
+        currentIndex = (currentIndex + 1) % images.length;
+        showImage(currentIndex);
+    }
+
+    setInterval(nextImage, 2000);
+    showImage(currentIndex);
+});
+
